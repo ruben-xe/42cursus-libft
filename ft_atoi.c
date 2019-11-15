6df419e2c69c12aff9b934fb442b6d72029f98c5
Richard Nixon Soto Inca <rsoto-in@student.42.fr>
2019-11-15 10:17:12 +0100

    Minor fixes related to 42's norme.

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,11 @@
 /*   By: rsoto-in <rsoto-in@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 21:14:12 by rsoto-in          #+#    #+#             */
-/*   Updated: 2019/11/15 10:13:34 by rsoto-in         ###   ########.fr       */
+/*   Updated: 2019/11/15 10:16:21 by rsoto-in         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	int		i;
 	int		j;
@@ -21,7 +21,7 @@ int ft_atoi(const char *str)
 	n = 0;
 	base = 1;
 	while (str[i] != '\0' && (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
-            || str[i] == '\f' || str[i] == '\r' || str[i] == ' '))
+		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' '))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
