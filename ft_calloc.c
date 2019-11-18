d695a2a9a9b9d8bd091bbbb18b52356b7a47de4a
Richard Nixon Soto Inca <rsoto-in@student.42.fr>
2019-11-18 17:43:47 +0100

    Remove function dependency ft_setzero from ft_calloc to simplify logic.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,31 +6,24 @@
 /*   By: rsoto-in <rsoto-in@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 10:28:26 by rsoto-in          #+#    #+#             */
-/*   Updated: 2019/11/15 15:52:58 by rsoto-in         ###   ########.fr       */
+/*   Updated: 2019/11/18 17:38:23 by rsoto-in         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_setzero(void *s, size_t n)
-{
-	char			*mem;
-	unsigned int	i;
-
-	mem = s;
-	i = -1;
-	while (mem && n > 0 && ++i >= 0 && i < n)
-		mem[i] = 0;
-}
-
 void		*ft_calloc(size_t count, size_t size)
 {
-	void	*mem;
+	char	*mem;
+	int		i;
 
-	if ((mem = (void *)malloc(count * size)))
+	mem = NULL;
+	if ((mem = malloc(count * size)))
 	{
-		ft_setzero(mem, count * size);
-		return (mem);
+		i = -1;
+		while (mem && count * size > 0 && ++i >= 0 &&
+				(unsigned int)i < count * size)
+			mem[i] = 0;
 	}
-	return (NULL);
+	return ((void *)mem);
 }
