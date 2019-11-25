c9732188d93839db82b4e7e5c3782f52465baf1a
Richard Nixon Soto Inca <rsoto-in@student.42.fr>
2019-11-25 16:32:18 +0100

    Fix crash in ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: rsoto-in <rsoto-in@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 18:12:54 by rsoto-in          #+#    #+#             */
-/*   Updated: 2019/11/25 14:35:13 by rsoto-in         ###   ########.fr       */
+/*   Updated: 2019/11/25 16:28:27 by rsoto-in         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ void	ft_lstadd_back(t_list **alst, t_list *new)
 
 	if (alst && new)
 	{
-		lst = *alst;
-		if (lst)
+		if (*alst)
 		{
+			lst = *alst;
 			while (lst->next)
 				lst = lst->next;
 			lst->next = new;
 		}
 		else
-			lst = new;
+			*alst = new;
 	}
 }
