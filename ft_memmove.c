c19987c6bc65cb2c0d26d615e424a1d09fe22120
Richard Nixon Soto Inca <rsoto-in@student.42.fr>
2020-01-11 12:53:59 +0100

    Fix some libft_basics functions.

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rsoto-in <rsoto-in@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 12:05:35 by rsoto-in          #+#    #+#             */
-/*   Updated: 2019/12/09 12:38:38 by rsoto-in         ###   ########.fr       */
+/*   Updated: 2020/01/11 12:49:55 by rsoto-in         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,20 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*d;
 	unsigned char	*s;
-	size_t			i;
+	int				size;
+	int				i;
 
 	if (!src && !dst)
 		return (NULL);
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	size = len;
+	i = -1;
+	if (d < s)
+		while (++i < size)
+			d[i] = s[i];
 	else
-	{
-		d = (unsigned char *)dst;
-		s = (unsigned char *)src;
-		i = 0;
-		if (d < s)
-			while (i < len)
-			{
-				d[i] = s[i];
-				i++;
-			}
-		else
-			while (--len >= 0)
-				d[len] = s[len];
-	}
+		while (--size >= 0)
+			d[size] = s[size];
 	return (dst);
 }
